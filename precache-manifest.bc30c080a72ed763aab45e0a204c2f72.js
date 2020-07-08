517e228db52c8f939e17e7e319dd57eb750bfe17
azeddine rokai <azeddinerokai@gmail.com>
2020-07-08 15:12:55 +0200

    Updates

---
+++ precache-manifest.bc30c080a72ed763aab45e0a204c2f72.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a72d3afdfd50cfa36cc655261eec155",
+    "url": "/todolist-/index.html"
+  },
+  {
+    "revision": "435282c8c12b0ddace62",
+    "url": "/todolist-/static/css/2.ba963f73.chunk.css"
+  },
+  {
+    "revision": "fb3ac9cb286e763d1766",
+    "url": "/todolist-/static/css/main.06a56685.chunk.css"
+  },
+  {
+    "revision": "435282c8c12b0ddace62",
+    "url": "/todolist-/static/js/2.83b717d3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist-/static/js/2.83b717d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3ac9cb286e763d1766",
+    "url": "/todolist-/static/js/main.f4cb0393.chunk.js"
+  },
+  {
+    "revision": "d47f2fc0ec8161da2f0f",
+    "url": "/todolist-/static/js/runtime-main.1baec6ec.js"
+  },
+  {
+    "revision": "917b73396db1c6648cf364541f37528e",
+    "url": "/todolist-/static/media/c-1.917b7339.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/todolist-/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/todolist-/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/todolist-/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/todolist-/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/todolist-/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
